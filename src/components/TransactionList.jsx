d002f9bb6a3aceb3e19dc465832d5e349978ce52
andre353 <eva595eva@gmail.com>
2021-12-25 21:23:02 +0300

    Add Transaction component, import it to TransactionList component

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import Transaction from "./Transaction";
 
 function TransactionList() {
+  // we can pull out anything from our context using the useContext hook
+  //const context = useContext(GlobalContext); // GlobalContext = createContext(dispatch(initialState))
+  // we can profit from array distructuring rather than to write context.transactions
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transactions.map((transaction) => (
+          <Transaction transaction={transaction} key={transaction.id}/>
+        ))}
       </ul>
     </>
   );
