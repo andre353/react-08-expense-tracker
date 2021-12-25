8385f9ff3276cf6e5a28e89eab08dbfd5bbbcb1e
andre353 <eva595eva@gmail.com>
2021-12-25 20:38:24 +0300

    Add useState hook to AddTransaction component, add useContext and useReducer to GlobalState, add appReducer file to manage app state changes

---
+++ src/context/AppReducer.js
@@ -0,0 +1,10 @@
+// here we specify the application state changes in response to certain actions to our store/context
+const appReducer = (state, action) => {
+    // action.type in react can be compared with id in vanilla js
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export default appReducer
