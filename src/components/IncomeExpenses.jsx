ca70c1663033f8c0a60857df19ebb10270da7744
andre353 <eva595eva@gmail.com>
2021-12-25 21:39:42 +0300

    Add total sum calculation to Balance component, add income and expense sums calc to IncomeExpenses component

--- src/components/IncomeExpenses.jsx
+++ src/components/IncomeExpenses.jsx
@@ -1,19 +1,36 @@
-import React from "react";
+import React, {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 function IncomeExpenses() {
+  const {transactions} = useContext(GlobalContext);
+
+  // an array of all amounts
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  // filter and sup up all positive numbers
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  // filter and sum up all negative numbers
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
   return (
     <>
       <div className="inc-exp-container">
         <div>
           <h4>Income</h4>
           <p className="money plus">
-            +$0.00
+            +${income}
           </p>
         </div>
         <div>
           <h4>Expense</h4>
           <p className="money minus">
-            -$0.00
+            -${expense}
           </p>
         </div>
       </div>
